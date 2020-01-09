926dcd519b86cc1b90e56459c87ae7b07e9749dd
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-09 13:48:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,10 @@
 class Dog 
-
-def name = (dog_name) 
- @name = dog_name  
-end 
-
-def name 
-  @name 
-end 
-  
+  def name = (dog_name) 
+  @name = dog_name  
+  end 
+  def name 
+     @name 
+  end 
   def breed=(breed)
     @breed = breed
   end
